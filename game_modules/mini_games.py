6b9ed3c5f05b1ef39be53720967db8d9871831f8
DScriptz <dwaynearthurj@gmail.com>
2025-12-09 11:27:34 +0800

    fix: adjusted the amount of the win_amount  when rolled 5 to  where the player wins the bet + 5 and also for jackpot but + 10

--- game_modules/mini_games.py
+++ game_modules/mini_games.py
@@ -47,14 +47,14 @@ def play_relic_dice(player):
         time.sleep(1.5)
 
     elif roll <= 5:
-        win_amount = int(bet_amount * 1.5)
+        win_amount = int(bet_amount * 1.5 + 5)
         player.stormmarks += win_amount
         print(f"The dice flickers a light! You won {win_amount} SMK!!")
         time.sleep(1.5)
 
     else:
 
-        jackpot = int(bet_amount * 3)
+        jackpot = int(bet_amount * 3 + 10)
         player.stormmarks += jackpot
         print(f"JACKPOT! The Dice shines! You win a MASSIVE {jackpot} SMK!")
 
