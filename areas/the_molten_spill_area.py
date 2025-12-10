9c4d6da055ce0d8629a7a95605168b4d2b7ad923
DScriptz <dwaynearthurj@gmail.com>
2025-12-10 14:35:18 +0800

    feat: Added new location The Molten Spill, and its movement code

---
+++ areas/the_molten_spill_area.py
@@ -0,0 +1,30 @@
+import time
+from colorama import Style, Fore
+from models import Location
+from world_movement.molten_spill_movement import handle_jump_over, handle_wait
+
+
+reset = Style.RESET_ALL
+
+def go_to_molten_spill(player):
+
+    print("\nAs you venture, you see a big, wide, fallen pipe spitting out very hot molten!")
+    time.sleep(1)
+    print(f"{player.name}: 'This is blocking my way... What should I do?'")
+
+    return the_molten_spill.enter(player)
+
+the_molten_spill = Location(
+    "The Molten Spill",
+    f"""\nThe Molten Spill, Your path is severed. 
+    Before you flows a glowing river of molten metal, slow and luminous, poured fresh from a fractured overhead pipe.
+    The crushing heat is immediate, and the thick, coppery scent of superheated ore chokes the air.
+    You are forced to halt, the distant metal *CLANGING* of the Foundry echoing your frustration.
+                 
+    Area Signal Tower Coverage: [{Fore.RED + Style.BRIGHT}II - HIGH Danger{reset}]   
+    """,
+    {
+        "1": ('Wait - Step back and wait for the metal to cool (slow but safe + 1 step)', handle_wait),
+        "2": ('Jump Over - Attempt to jump over the Hot Molten (Risky but faster + 5 steps if success)', handle_jump_over)
+    }
+)
---
+++ world_movement/molten_spill_movement.py
@@ -0,0 +1,44 @@
+import random
+import time
+
+
+def handle_jump_over(player):
+
+    if random.random() <= 0.50:
+        molten_damage = random.randint(10, 20)
+
+        if player.health > 0:
+            player.health -= molten_damage
+        else:
+            player.health = 0
+
+        print("\nYou jump over but your feet touched the Molten Lava!!")
+        time.sleep(0.8)
+        print(f"{player.name}: 'Ow! That burns, aghhh'")
+        time.sleep(1)
+        print(f"\nYou got burned for: {molten_damage}! Health is now: {player.health}/{player.max_health}")
+        player.location_steps += 1
+        print(f"You reached {player.location_steps} step(s) now.")
+
+    else:
+        player.location_steps += 5
+        print("You jump over safely!")
+        time.sleep(0.8)
+        print(f"{player.name}: 'Phew, that was close...'")
+        time.sleep(1)
+        print(f"You reached {player.location_steps} step(s) now.")
+
+    return player
+
+def handle_wait(player):
+    player.location_steps += 1
+    print("You took a step back and waiting for the Molten Lava to cool down a bit..")
+    time.sleep(1.1)
+    print(f"{player.name}: 'Whew, okay this might be smart...'")
+    time.sleep(0.7)
+    print("As the Molten sort of cools down, you got a thick metal sheet and placed it over the molten and walked on "
+          "top of it cautiously")
+    time.sleep(1.2)
+    print(f"You reached {player.location_steps} step(s) now.")
+
+    return player
